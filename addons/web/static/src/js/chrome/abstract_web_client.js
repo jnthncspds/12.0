ffbfa43f69fd3f1e9b640c575e122594c193d2ed
Jonathan Cespedes <cespedesjonathan25@gmail.com>
2019-03-28 09:21:04 -0400

    changing name of tabs

--- addons/web/static/src/js/chrome/abstract_web_client.js
+++ addons/web/static/src/js/chrome/abstract_web_client.js
@@ -98,7 +98,7 @@ var AbstractWebClient = Widget.extend(ServiceProviderMixin, KeyboardNavigationMi
         this._current_state = null;
         this.menu_dm = new concurrency.DropMisordered();
         this.action_mutex = new concurrency.Mutex();
-        this.set('title_part', {"zopenerp": "Odoo"});
+        this.set('title_part', {"zopenerp": "034-Fashion"});
     },
     start: function () {
         var self = this;
